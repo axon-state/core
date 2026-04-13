cc122aa92940de6b3a6bc771bba3893835baea87
Marco Buschini <marco.buschini@gmail.com>
2026-04-13 18:13:01 +0200

    feat: add husky and committizen to the workflow

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,21 @@ const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
 
 module.exports = defineConfig([
+  // 1. GLOBAL IGNORES BLOCK (Must be a standalone object with ONLY 'ignores')
+  {
+    ignores: [
+      "**/node_modules/",
+      "**/dist/",
+      "**/coverage/",
+      "**/.angular/",
+      "**/axon.ts.html", // Explicitly kill the offender
+    ],
+  },
+
+  // 2. TypeScript Config
   {
     files: ['**/*.ts'],
+    // Removed ignores from here so they don't conflict with global ones
     extends: [
       eslint.configs.recommended,
       tseslint.configs.recommended,
@@ -17,22 +30,16 @@ module.exports = defineConfig([
     rules: {
       '@angular-eslint/directive-selector': [
         'error',
-        {
-          type: 'attribute',
-          prefix: 'lib',
-          style: 'camelCase',
-        },
+        { type: 'attribute', prefix: 'lib', style: 'camelCase' },
       ],
       '@angular-eslint/component-selector': [
         'error',
-        {
-          type: 'element',
-          prefix: 'lib',
-          style: 'kebab-case',
-        },
+        { type: 'element', prefix: 'lib', style: 'kebab-case' },
       ],
     },
   },
+
+  // 3. HTML Config
   {
     files: ['**/*.html'],
     extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
